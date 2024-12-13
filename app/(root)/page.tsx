bebe90a1894c0e8bbcaf197af484386f94f84611
BK <kbomin328@gmail.com>
2024-12-14 01:05:42 +0900

    Feat: Home Page UI
    - HeaderBox
    - TotalBalanceBox
    - AnimatedCounter
    - DoughnutChart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox"
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 
 const Home = () => {
+  const loggedIn = { firstName: 'Kwon'};
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Gust'}
+            subtext="계좌와 거래 내역을 편리하게 접근하고 관리할 수 있습니다."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
