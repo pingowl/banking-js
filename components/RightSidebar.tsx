e2567647fa2d07270a5d5e449f6d0a607685da87
BK <kbomin328@gmail.com>
2024-12-18 18:45:30 +0900

    Feat: RightSidebar - 소비 카테고리

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image"
 import Link from "next/link"
 import BankCard from './BankCard'
+import { countTransactionCategories } from '@/lib/utils'
+import Category from "./Category"
 
 const RightSidebar = ({user, transactions, banks}:RightSidebarProps) => {
+
+  const categories: CategoryCount[] = countTransactionCategories(transactions);
+
   return (
     <aside className="right-sidebar">
         <section className="flex flex-col pb-8">
@@ -62,7 +67,15 @@ const RightSidebar = ({user, transactions, banks}:RightSidebarProps) => {
           </div>
         )}
 
-      </section>
+          <div className="mt-10 flex flex-1 flex-col gap-6">
+            <h2 className="header-2">상위 카테고리</h2>
+            <div className='space-y-5'>
+              {categories.map((category, index) => (
+                <Category key={category.name} category={category} />
+              ))}
+            </div>
+          </div>
+        </section>
     </aside>
   )
 }
